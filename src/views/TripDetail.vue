8a21f00d8bf9cd1647a10e7c22f06e7da162ac56
chen321 <1061025302@qq.com>
2025-12-19 16:46:29 +0800

    dev

--- src/views/TripDetail.vue
+++ src/views/TripDetail.vue
@@ -13,7 +13,7 @@
         </div>
         <div class="header-right">
           <!-- <el-button size="mini" @click="saveTrip">保存</el-button> -->
-          <el-button size="mini" @click="shareTrip">分享</el-button>
+          <!-- <el-button size="mini" @click="shareTrip">分享</el-button> -->
           <el-button size="mini" @click="exportVisible = true">导出</el-button>
           <el-button size="mini" @click="showPlaceSearch = true">添加地点</el-button>
         </div>
